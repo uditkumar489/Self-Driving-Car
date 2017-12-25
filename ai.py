c8475137d9320c5aee4080357c30bbc02be0f0b9
udit kumar <uditkumar489@gmail.com>
2017-12-25 17:09:57 +0530

    forward/activation function created
    this will activate hidden and output layer neurons

--- ai.py
+++ ai.py
@@ -22,15 +22,21 @@ from torch.autograd import Variable  #to convert tensors into variables and grad
 
 #AIM_1 -> To create neural network 
  #Our Neural-Network class will have 2 functions :
-
-
+ #1. init func()    - to define the structure of the NN
+ #2. forward func() - to activate the neurons and return q_values for each possible action
+ 
 class Network(nn.Module):
     
-    def __init__(self, input_size, nb_action):
-        super(Network, self).__init__()       #trick to inherit all the properties of "nn.Module" in single shot
-        self.input_size = input_size          #self.input_size is just a way to attact a variable to the input layer of neuralN. 
+    def __init__(self, input_size, nb_action):  #'special' syntax to define structure
+        super(Network, self).__init__()         #trick to inherit all the properties of "nn.Module" in single shot
+        self.input_size = input_size            #self.input_size is just a way to attact a variable to the input layer of neuralN. 
         #Note : above line signifies no. of input neuron = input size (in this case i.e 5)
-        self.nb_action = nb_action            #same as above case but in o/p neurons
-        self.fc1 = nn.Linear(input_size, 30)  #full connection b/w i/p and hidden layer
-        self.fc2 = nn.Linear(30, nb_action)   #full connection b/w hidden and o/p layer
+        self.nb_action = nb_action              #same as above case but in o/p neurons
+        self.fc1 = nn.Linear(input_size, 30)    #full connection b/w i/p and hidden layer
+        self.fc2 = nn.Linear(30, nb_action)     #full connection b/w hidden and o/p layer
         
+        
+    def forward(self, state):        #ofcourse , states are the inputs of our NN
+        x = F.relu(self.fc1(state))  #activating hidden_layer neurons
+        q_values = self.fc2(x)       #activating o/p neurons
+        return q_values
